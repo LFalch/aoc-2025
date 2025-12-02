1edf7a7c368395ec92569e7545d486cf0a83ec84
LFalch <lucas@wasd.dk>
2025-12-02 20:30:20 +0100

    day 2: Remove slow powl function

--- day02/main.c
+++ day02/main.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <math.h>
 #include <stdbool.h>
 #include <sys/types.h>
-#define _GNU_SOURCE 1
-#define _DEFAULT_SOURCE 1
 
-//   4174379265
-// 455439374640
-
-uint64_t numPlaces (uint64_t n);
-uint64_t sumInvalids(uint64_t from, uint64_t to) {
+static uint64_t numPlaces(uint64_t n);
+static uint64_t pow10_(uint64_t n);
+static uint64_t sumInvalids(uint64_t from, uint64_t to) {
     uint64_t sum = 0;
     for (uint64_t n = from; n <= to; n++) {
         uint32_t places = numPlaces(n);
         if (places % 2 == 0) {
-            uint64_t digits = powl(10, places / 2);
+            uint64_t digits = pow10_(places / 2);
             if (n / digits == n % digits) {
                 sum += n;
             }
@@ -24,14 +19,13 @@ uint64_t sumInvalids(uint64_t from, uint64_t to) {
     }
     return sum;
 }
-uint64_t sumInvalidsExt(uint64_t from, uint64_t to, uint64_t *simpleSum) {
-    uint64_t sum = 0;
+static void sumInvalidsExt(uint64_t from, uint64_t to, uint64_t *extSum, uint64_t *simpleSum) {
     for (uint64_t n = from; n <= to; n++) {
         uint32_t places = numPlaces(n);
         for (uint32_t pl = places / 2; pl > 0; pl--) {
             if (places % pl != 0) continue;
             uint32_t reps = places / pl;
-            uint64_t digits = powl(10, pl);
+            uint64_t digits = pow10_(pl);
 
             uint64_t val = n;
             uint32_t part = n % digits;
@@ -48,12 +42,11 @@ uint64_t sumInvalidsExt(uint64_t from, uint64_t to, uint64_t *simpleSum) {
             if (invalid) {
                 if (pl == places / 2 && places % 2 == 0)
                     *simpleSum += n;
-                sum += n;
+                *extSum += n;
                 break;
             }
         }
     }
-    return sum;
 }
 
 int main(int argc, char* argv[]) {
@@ -66,13 +59,13 @@ int main(int argc, char* argv[]) {
             return -1;
         }
         // invalidSum += sumInvalids(from, to);
-        invalidSumExt += sumInvalidsExt(from, to, &invalidSum);
+        sumInvalidsExt(from, to, &invalidSumExt, &invalidSum);
     } while (fgetc(f) == ',');
-    printf("invalidSum: %lu\n", invalidSum);
-    printf("invalidSumExt: %lu\n", invalidSumExt);
+    printf("part 1: %lu\n", invalidSum);
+    printf("part 2: %lu\n", invalidSumExt);
 }
 
-uint64_t numPlaces (uint64_t n) {
+static uint64_t numPlaces(uint64_t n) {
     if (n < 10) return 1;
     if (n < 100) return 2;
     if (n < 1000) return 3;
@@ -83,4 +76,19 @@ uint64_t numPlaces (uint64_t n) {
     if (n < 100000000) return 8;
     if (n < 1000000000) return 9;
     return 9 + numPlaces(n / 1000000000);
+}
+static uint64_t pow10_(uint64_t n) {
+    switch (n) {
+        case 0: return 1;
+        case 1: return 10;
+        case 2: return 100;
+        case 3: return 1000;
+        case 4: return 10000;
+        case 5: return 100000;
+        case 6: return 1000000;
+        case 7: return 10000000;
+        case 8: return 100000000;
+        case 9: return 1000000000;
+        default: return 10000000000 * pow10_(n-10);
+    }
 }
